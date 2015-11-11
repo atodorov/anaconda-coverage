c055018aeceffa4082349b543e0ec48b7430ab4f
Alexander Todorov <atodorov@redhat.com>
2015-11-11 20:53:40 +0200

    Add logging to journalctl

--- update.sh
+++ update.sh
@@ -26,7 +26,7 @@ for f in ".coverage" \
     tgt=`basename $f`
     wget "$JENKINS/job/anaconda-x86_64/lastSuccessfulBuild/artifact/$f" -O "$tgt" 2>/dev/null
     if [ "$?" -ne "0" ]; then
-        echo "Downloading $f failed, exitting ..."
+        echo "Downloading $f failed ..." | systemd-cat -t $DIRNAME -p err
 # don't exit if some of the downloads fail
 #        exit 2
     fi
@@ -39,6 +39,7 @@ if [ -n "$DIFF" ]; then
     git commit -a -m "Updated coverage results from $DATE"
     git tag "anaconda-$DATE"
     git push --tags origin master
+    echo "git push" | systemd-cat -t $DIRNAME -p info
 fi
 
 popd
