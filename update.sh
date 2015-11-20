2a7137ba169640b2f31a2f47eae627ff8c6e83c6
Alexander Todorov <atodorov@redhat.com>
2015-11-20 16:46:06 +0200

    Don't commit modified files after failed download

--- update.sh
+++ update.sh
@@ -27,8 +27,7 @@ for f in ".coverage" \
     wget "$JENKINS/job/anaconda-x86_64/lastSuccessfulBuild/artifact/$f" -O "$tgt" 2>/dev/null
     if [ "$?" -ne "0" ]; then
         echo "Downloading $f failed ..." | systemd-cat -t $DIRNAME -p err
-# don't exit if some of the downloads fail
-#        exit 2
+        git checkout $tgt
     fi
 done
 
@@ -36,7 +35,7 @@ DATE=`date --rfc-3339=date`
 DIFF=`git diff`
 # changes detected
 if [ -n "$DIFF" ]; then
-    git commit -a -m "Updated coverage results from $DATE"
+    git commit -a -m "Updated test results from $DATE"
     git tag "anaconda-$DATE"
     git push --tags origin master
     echo "git push" | systemd-cat -t $DIRNAME -p info
